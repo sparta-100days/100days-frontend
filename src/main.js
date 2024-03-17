47e8128a39949874897f4acee7f931e7d90b6adc
YJ9352 <act07612@gmail.com>
2024-03-18 02:32:20 +0900

    🧹Remove: 기존 템플릿 삭제

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// Nucleo Icons
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
 
-import materialKit from "./material-kit";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-app.use(materialKit);
-app.mount("#app");
+createApp(App).mount('#app')
