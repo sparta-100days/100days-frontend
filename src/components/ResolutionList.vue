bad8ea808f21ae112bbb307123a3742bdd783c45
Bang <threejun1007@gmail.com>
2024-03-20 02:23:04 +0900

    ⭐feat: 목표 리스트 기본 틀 작성

---
+++ src/components/ResolutionList.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div class="resolution-list">
+    <h2>목표 목록</h2>
+    <ul>
+      <li v-for="resolution in testList" :key="resolution.id">
+        <p class="resolution-info">
+          {{ resolution.title }}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      testList: [
+        { title: "test1", category: "test1", likeCount: 1 },
+        { title: "test2", category: "test2", likeCount: 2 },
+        { title: "test3", category: "test3", likeCount: 3 },
+      ],
+    };
+  },
+};
+</script>
+
+<style src="../assets/css/ResolutionList.css" lang="css"></style>
