7ece80a42f569f0520a105ee8bba33795c1f5fbc
YJ9352 <act07612@gmail.com>
2024-03-30 11:00:32 +0900

    ⭐Feat: 메일 인증 페이지 1-1. 메일주소를 입력하지 않았을 때 에러메세지를 추가했습니다. 1-2. 에러메세지가 3초 뒤 사라지게 했습니다. 2. 메세지 추가에 따른 창 넓이가 자동으로 조절되게 했습니다.

--- src/components/EmailVerificationPopup.vue
+++ src/components/EmailVerificationPopup.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="email-verification-popup">
-    <div class="popup-content">
+    <div class="popup-content" :style="{ height: popupHeight }">
       <h2 class="popup-title">이메일 인증</h2>
-      <input type="email" v-model="emailInput" required placeholder="Enter your email">
+      <input type="email" v-model="emailInput" required placeholder="메일주소를 입력해주세요.">
       <button @click="sendVerificationCode">
         {{ isCodeSent ? "인증번호 재전송" : "인증 번호 보내기" }}
       </button>
       <!-- Timer and Error Message Display -->
       <div v-if="isCodeSent">
-        <span>{{ Math.floor(timeLeft / 60) }}:{{ ('0' + (timeLeft % 60)).slice(-2) }}</span>
+        <span class="timeer">{{ Math.floor(timeLeft / 60) }}:{{ ('0' + (timeLeft % 60)).slice(-2) }}</span>
         <span v-if="errorMessage" class="error-message">{{ errorMessage }}</span>
       </div>
-      <input type="text" v-model="verificationCodeInput" placeholder="Enter verification code">
+      <input type="text" v-model="verificationCodeInput" placeholder="인증번호를 입력해주세요.">
       <button @click="verifyCode">인증하기</button>
       <button @click="closePopup">닫기</button>
     </div>
@@ -28,13 +28,23 @@ export default {
       timer: null,
       timeLeft: 5 * 60, // 5 minutes in seconds
       errorMessage: "",
+      popupHeight: '400px', // 초기 높이 설정
     };
   },
   methods: {
     sendVerificationCode() {
       this.isCodeSent = true;
       this.startTimer();
-      // 여기에 이메일로 인증 코드 전송 로직 구현 (인증 코드는 123456으로 가정)
+      // 이메일로 인증 코드 전송 로직을 구현하세요
+      // 예: this.fakeSendVerificationCode();
+      this.popupHeight = '460px'; // 인증번호 보내기 버튼을 눌렀을 때 높이 변경
+
+      if (!this.emailInput.trim()) {
+        this.errorMessage = "메일주소를 입력해주세요."
+        setTimeout(() => {
+          this.errorMessage = ""
+          }, 3000);
+      }
     },
     startTimer() {
       if (this.timer) {
@@ -56,6 +66,9 @@ export default {
         clearInterval(this.timer);
       } else {
         this.errorMessage = "인증 번호가 올바르지 않습니다.";
+        setTimeout(() => {
+          this.errorMessage = ""; // 일정 시간 후에 에러 메시지 초기화
+        }, 5000); // 5초 후에 초기화 (5000ms)
       }
     },
     closePopup() {
