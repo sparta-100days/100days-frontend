c5baf2c637afaee5951947bf5b000ee4871ee31c
rim109 <fla7722@naver.com>
2024-03-31 22:52:35 +0900

    🧹Remove: 상세페이지 임포트 삭제

--- src/views/message/MessageMain.vue
+++ src/views/message/MessageMain.vue
@@ -34,7 +34,6 @@ import  Tab1  from "@/components/message/MessageWrite.vue";
 import  Tab2  from "@/components/message/MessageSend.vue";
 import  Tab3  from "@/components/message/MessageReceived.vue";
 import  Tab4  from "@/components/message/MessageAdmin.vue";
-import MessageDetail from "@/components/message/MessageDetail.vue";
 
 export default {
   data() {
