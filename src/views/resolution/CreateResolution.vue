a5f53e47783fd6d0c9ea908a42c90f28c0dd6d91
YJ9352 <act07612@gmail.com>
2024-03-31 09:10:10 +0900

    ⭐Feat: 목표작성페이지 완성

--- src/views/resolution/CreateResolution.vue
+++ src/views/resolution/CreateResolution.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="create-resolution">
-    <h2>새 목표 만들기</h2>
-    <form @submit.prevent="submitForm">
-      <div class="form-group">
-        <label for="title">목표</label>
-        <input type="text" id="title" v-model="resolution.title" required />
-        <span>{{ validationMessages.title }}</span>
-      </div>
-      <div class="form-group">
-        <label for="description">설명</label>
-        <textarea id="description" v-model="resolution.description" required></textarea>
-        <span>{{ validationMessages.description }}</span>
-      </div>
-      <div class="form-group">
-        <label for="category">카테고리</label>
-        <input type="text" id="category" v-model="resolution.category" required>
-        <span>{{ validationMessages.category }}</span>
-      </div>
-      <button type="submit">목표 생성</button>
-    </form>
+  <div class="create-resolution-background">
+    <div class="create-resolution-container">
+      <div class="headlight"></div>
+      <h2>새 목표 만들기</h2>
+      <form @submit.prevent="submitForm">
+        <div class="form-group">
+          <input class="create-resolution-title" type="text" id="title" v-model="resolution.title" required />
+          <span>{{ validationMessages.title }}</span>
+        </div>
+        <div class="form-group">
+          <textarea class="create-resolution-description" id="description" v-model="resolution.description" required></textarea>
+          <span>{{ validationMessages.description }}</span>
+        </div>
+        <div class="form-group">
+          <input type="text" id="category" v-model="resolution.category" required>
+          <span>{{ validationMessages.category }}</span>
+        </div>
+        <button type="submit" class="create-resolution-btn">목표 생성</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -45,9 +45,9 @@ export default {
       // 폼 데이터가 유효하면 백엔드 API로 데이터를 전송하는 로직을 추가합니다.
       console.log("Form submitted:", this.resolution);
       // API 호출 로직은 여기에 추가합니다.
-    }
+    },
   }
 };
 </script>
 
-<style src="../../assets/css/resolution/CreateResolution.css" lang="css"></style>
+<style src="@/assets/css/resolution/CreateResolution.css" lang="css"></style>
