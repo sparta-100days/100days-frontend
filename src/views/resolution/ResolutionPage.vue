7f017344db1c94461466ac9773a5c2b149af8483
YJ9352 <act07612@gmail.com>
2024-03-31 19:53:04 +0900

    ⭐Feat: 목표 상세페이지 틀 작성

--- src/views/resolution/ResolutionPage.vue
+++ src/views/resolution/ResolutionPage.vue
@@ -1,21 +1,40 @@
 <template>
-  <div class="resolution">
-    <h2>제목</h2>
-    <p>사용할 id: {{ resolutionId }}</p>
-    <div class="info-box">
-      <p class="description">설명</p>
-      <p class="progress">진행도</p>
+  <div class="mainpage-resolution-background">
+    <i class="fas fa-moon"></i>
+    <i class="fas fa-moon blur"></i>
+    <div class="mainpage-resolution-container">
+      <div class="mainpage-resolution-box">
+        <div class="main-resol-title-box">
+          <h2 class="main-resol-title">작성 목표 제목</h2>
+          <p class="main-resol-userId">| {{ resolutionId }}</p>
+        </div>
+        <div class="mainpage-resolution-description-box">
+          <p class="mainpage-resolution-description">
+            설명 : 예시를위해길게적어볼게요예시를위해길게적어볼게요
+            예시를위해길게적어볼게요예시를위해길게적어볼게요
+            예시를위해길게적어볼게요예시를위해길게적어볼게요
+            예시를위해길게적어볼게요예시를위해길게적어볼게요
+            예시를위해길게적어볼게요예시를위해길게적어볼게요
+          </p>
+        </div>
+        <div class="mainpage-resolution-progress-box">
+          <div class="progress-container">
+            <div class="progress-bar" :style="{ width: progress + '%' }">{{ progress }}%</div>
+          </div>
+        </div>
+        <div class="mainpage-resolution-daily-status-box">
+          <div class="mainpage-resolution-d-day-box">
+            <p class="mainpage-resolution-like-check">♥</p>
+            <footer class="footer" v-if="isAuthor">
+              <button class="resol-feedback-button">데일리 체크 하러 가기</button>
+            </footer>
+          </div>
+        </div>
+        <div class="mainpage-resolution-daily-check">
+          <!-- 달력 형식이나 스크롤 형식 컴포넌트 추가 -->
+        </div>
+      </div>
     </div>
-    <div class="daily-ckeck-box">
-      <p class="d-day">d - 100</p>
-      <p class="daily-status">오늘 목표 완료</p>
-    </div>
-    <div class="daily-check">
-      <p>달력 형식 or 스크롤 형식</p>
-    </div>
-    <footer class="footer" v-if="isAuthor">
-      <button class="feedback-button">데일리 체크 하러 가기</button>
-    </footer>
   </div>
 </template>
 
@@ -36,17 +55,25 @@ export default {
       resolutionId,
     };
   },
-  mounted() {
-    this.checkAuthor();
-  },
   data() {
     return {
+      progress: 0, // 초기 프로그레스 값
       isAuthor: false,
     };
+  },
+  mounted() {
+    this.checkAuthor();
+    setInterval(() => {
+      const randomValue = Math.floor(Math.random() * 101); // 0부터 100까지의 랜덤한 값
+      this.updateProgressBar(randomValue);
+    }, 1000); // 1초마다 업데이트
   },
   methods: {
     checkAuthor() {
       this.isAuthor = true;
+    },
+    updateProgressBar(data) {
+      this.progress = data;
     },
   },
 };
